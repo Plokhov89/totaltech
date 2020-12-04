688835d8258792c1fefcb80642be6fe97d44f8a4
Ilya Plokhov <plokhov89@gmail.com>
2020-12-04 11:58:12 +0200

    Изменение оформления

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*Бургер меню-->*/
 $(document).ready(function() {
     $('.header_burger').click(function(event) {
         $('.header_burger,.header_menu').toggleClass('active');
@@ -5,6 +6,7 @@ $(document).ready(function() {
     });
 });
 
+/*Прокрутка страницы*/
 $(document).ready(function(){
     $("#menu").on("click","a", function (event) {
         event.preventDefault();
