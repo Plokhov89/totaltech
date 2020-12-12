63aa4e1c290700732eb09e864acf98a2630d7963
Ilya Plokhov <plokhov89@gmail.com>
2020-12-12 13:02:24 +0200

    Оптимизация CSS, fix мобильной версии

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*Бургер меню-->*/
 $(document).ready(function() {
-    $('.header_burger').click(function(event) {
-        $('.header_burger,.header_menu').toggleClass('active');
+    $('.header-burger').click(function(event) {
+        $('.header-burger,.header-menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
 });
@@ -15,3 +15,16 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1500);
     });
 });
+
+// Fix масштабирования экрана в мобильной версии
+$(function() {
+    if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+      var viewportmeta = document.querySelector('meta[name="viewport"]');
+      if (viewportmeta) {
+        viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
+        document.body.addEventListener('gesturestart', function () {
+          viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+        }, false);
+      }
+    }
+  })
